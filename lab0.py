b7fa0153ca1af2ab9604b640ed8b4507cf5e0b66
David Harting <david@emplify.com>
2018-04-14 15:52:32 -0400

    make sure matplotlib works

--- lab0.py
+++ lab0.py
@@ -7,6 +7,10 @@ Created on Sat Apr 14 14:45:51 2018
 """
 from graphviz import Digraph
 
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
 def assembleKnights():
     roundTable = Digraph(comment="The Round Table")
     roundTable.node('A', 'King Arthur')
@@ -16,6 +20,29 @@ def assembleKnights():
     roundTable.edge('B', 'L', constraint='false')
     return roundTable
 
+def plot():
+    """
+    This example is just straight up copy-pasted from
+    https://matplotlib.org/gallery/lines_bars_and_markers/simple_plot.html
+    I really just wanted to make sure my environment was correctly set up
+    to render matplotlib plots, and it is!
+    """
+
+    # Data for plotting
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
+
+    # Note that using plt.subplots below is equivalent to using
+    # fig = plt.figure() and then ax = fig.add_subplot(111)
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
+
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+        title='About as simple as it gets, folks')
+    ax.grid()
+
+    fig.savefig("output/plot.png")
+
 def main():
     x = "hello world"
     print(x)
@@ -23,6 +50,8 @@ def main():
     knights = assembleKnights()
     print(knights.source)
     knights.render('output/round-table.gv', view=False)
+
+    plot()
 
 if __name__ == "__main__":
     main()
